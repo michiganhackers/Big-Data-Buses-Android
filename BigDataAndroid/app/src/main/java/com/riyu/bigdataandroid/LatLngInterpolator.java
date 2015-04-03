a7dc3ea69312b11da20b280634907c3a378f81fb
Ankit Shah <ank.h.shah@gmail.com>
2015-04-02 20:32:37 -0400

    Testing marker animation

---
+++ BigDataAndroid/app/src/main/java/com/riyu/bigdataandroid/LatLngInterpolator.java
@@ -0,0 +1,18 @@
+package com.riyu.bigdataandroid;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by ankit on 4/2/15.
+ */
+public interface LatLngInterpolator {
+    public LatLng interpolate (float fraction, LatLng a, LatLng b);
+
+    public class Linear implements  LatLngInterpolator{
+        public LatLng interpolate(float fraction, LatLng a, LatLng b){
+            double lat = (b.latitude - a.latitude) * fraction + a.latitude;
+            double lng = (b.longitude - a.longitude) * fraction + a.longitude;
+            return new LatLng(lat, lng);
+        }
+    }
+}
--- BigDataAndroid/app/src/main/java/com/riyu/bigdataandroid/MapsActivity.java
+++ BigDataAndroid/app/src/main/java/com/riyu/bigdataandroid/MapsActivity.java
@@ -1,32 +1,52 @@
 package com.riyu.bigdataandroid;
 
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.util.Property;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private UiSettings mUISettings;
     private LatLng ANN_ARBOR = new LatLng(42.2818294, -83.7317954);
+    private LatLng FINAL_POS = new LatLng(43, -82);
+    private float Lat = (float) 42.2818294;
+    private float Long = (float) -83.7317954;
+//    private LatLng someLat = new LatLng(42.2818294, -83.7317954);
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        SupportMapFragment mapFragment = ((SupportMapFragment)getSupportFragmentManager().
+                findFragmentById(R.id.map));
+//        assert(mapFragment!=null);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         
         setUpMapIfNeeded();
@@ -73,7 +93,47 @@ public class MapsActivity extends FragmentActivity {
         mMap.setMyLocationEnabled(true);
 //        mUISettings.setZoomControlsEnabled(true);
 //        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+//        addingBuses();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map){
+//        addingBuses();
+        Bitmap origBus = BitmapFactory.decodeResource(getResources(), R.drawable.temp_bus);
+        Bitmap scaledBus = Bitmap.createScaledBitmap(origBus, origBus.getWidth()/10, origBus.getHeight()/10, false);
+        Marker marker = map.addMarker(new MarkerOptions().position(ANN_ARBOR)
+                .title("Swishigan")
+                .icon(BitmapDescriptorFactory.fromBitmap(scaledBus)));
+        animateMarker(marker, FINAL_POS, new LatLngInterpolator.Linear());
+        Log.d("test", "yee");
     }
 
 
+    static void animateMarker(Marker marker, LatLng finalPos, final LatLngInterpolator latLngInterpolator){
+        TypeEvaluator<LatLng> typeEvaluator = new TypeEvaluator<LatLng>() {
+            @Override
+            public LatLng evaluate(float fraction, LatLng startValue, LatLng endValue) {
+                return latLngInterpolator.interpolate(fraction, startValue, endValue);
+            }
+        };
+        Property<Marker, LatLng> property = Property.of(Marker.class, LatLng.class, "position");
+        ObjectAnimator animator = ObjectAnimator.ofObject(marker, property, typeEvaluator, finalPos);
+        animator.setDuration(30000);
+        animator.start();
+    }
+
+   /* private void addingBuses(){
+        GroundOverlayOptions bus1 = new GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.temp_bus))
+                .position(ANN_ARBOR, 100, 100);
+        GroundOverlay imageOverlay = mMap.addGroundOverlay(bus1);
+        imageOverlay.setVisible(true);
+//        imageOverlay.setTransparency(1);
+//        for(int i =0; i<10000; i++){
+//            Lat+=.001;
+//            Long+=.001;
+//            bus1.position(new LatLng(Lat, Long), 100, 100);
+//            GroundOverlay imageOverlay = mMap.addGroundOverlay(bus1);
+//            imageOverlay.setVisible(true);
+//        }*/
 }
